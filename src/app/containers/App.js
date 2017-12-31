e6d89c5056b808fd9d25a2ea69a2de563cc5ffa2
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-12-31 09:51:13 -0200

    Project setup

---
+++ src/app/containers/App.js
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import AppBody from './AppBody';
+import session from '../../services/session';
+
+// Stubbed components
+const HomePage = () => <p>Home</p>;
+const NotFoundPage = () => <p>Not found</p>;
+
+const PrivateRoute = ({ component: Component, ...childProps}) => {
+	return (
+		<Route
+			{...childProps}
+			render={(props) => session.isAuthenticated() ? 
+				<Component {...props} /> : 
+				<Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+		/>
+	);
+};
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<AppBody>
+					<Switch>
+
+						<Route exact path="/" component={HomePage} />
+						<Route path="/login" component={HomePage} />
+						<PrivateRoute path="/private" component={HomePage} />
+						<Route component={NotFoundPage} />
+
+					</Switch>
+				</AppBody>
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+	children: PropTypes.element
+};
+
+export default App;
